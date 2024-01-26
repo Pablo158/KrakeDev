384d5070f33a07081b3493e266bdd1a15a707a1d
Actualizar <cordovapablo17@gmail.com>
2024-01-26 01:07:58 -0500

    modificamos la imagen

--- ejercicioHit/ejercicio.js
+++ ejercicioHit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar = function (){
+    let cmpImagenSaludo;
+    cmpImagenSaludo = document.getElementById("imgSaludi") // recuperamos la imagen
+    cmpImagenSaludo.src= "./imagenes/gifanimado.gif"
     console.log("sakudar")
 }
