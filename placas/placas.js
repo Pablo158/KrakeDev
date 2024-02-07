cc88e5ba696add21fc055a53d8fb88a8fd4960ec
Actualizar <cordovapablo17@gmail.com>
2024-02-07 10:40:17 -0500

    Corregido reto 35

--- placas/placas.js
+++ placas/placas.js
@@ -7,34 +7,49 @@ validarPlaca=function(){
     if(erroresEstructura==null){
         
         cambiarTexto("lblValidar","ESTRUCURA VALIDA");
+        //console.log(cambiarTexto("lblValidar","Corre bien"))
         let provincia = obtenerProvincia(placa);
 
         if (provincia !== null) {
             
             cambiarTexto("lblProvincia", "Provincia: " + provincia);
+            //console.log(cambiarTexto("lblProvincia","Corre bien"+provincia))
+
         } else {
             
             cambiarTexto("lblProvincia", "Provincia incorrecta");
+            //console.log(cambiarTexto("lblProvincia","Corre bien"))
         }
 
         let tipo=obtenerTipoVehiculo(placa);
 
         if(tipo !== null){
-            cambiarTexto("lblVehiculo", "Tipo de Vehiculo: ");
+            cambiarTexto("lblVehiculo", "Tipo de Vehiculo: "+tipo);
+           // console.log(cambiarTexto("lblVehiculo","tipo vehiculo "))
+
         }else{
             cambiarTexto("lblVehiculo", "Vehiculo Incorrecto");
+           // console.log(cambiarTexto("lblVehiculo"," vehiculo malo"))
         }
         let dia=obtenerDiaPicoYPlaca(placa,placa.length===8);
 
         if(dia!==null){
-            cambiarTexto("lblDia", "No circula: ");
+            cambiarTexto("lblDia", "No circula: "+dia);
+            //console.log(cambiarTexto("lblDia"," no circula"))
+
         }else{
             cambiarTexto("lblDia","Ultimo digito incorrecto");
+            //console.log(cambiarTexto("lblDia"," ultimo digito Incorrecto"))
+
         }
 
     }else{
         cambiarTexto("lblValidar","ESTRUCTURA INCORRECTA");
+        //console.log(cambiarTexto("lblValidar"," STRUCTURA INCORRECTA"))
+
         cambiarTexto("lblErrores",erroresEstructura);
+        //console.log(cambiarTexto("lblErrores",erroresEstructura))
+
         
     }
 }
--- placas/utilitarios.js
+++ placas/utilitarios.js
@@ -6,8 +6,8 @@ recuperarTexto=function(idComponente){
     valorIngresado=componente.value;
     return valorIngresado;
 }
-cambiarTexto = function(idComponente, mensaje){
-    let componente = document.getElementById(idComponente);
-    //Value limpia la caj de texto nombre
-    componente.value = mensaje;
+cambiarTexto=function(idComponente,mensaje){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.innerText = mensaje;
 }
