19c44cc8e02d22bb14b531f91de925f0f3d5a295
Actualizar <cordovapablo17@gmail.com>
2024-02-10 12:25:49 -0500

    Ejercio 37 for

---
+++ bucles/ejemploFor.js
@@ -0,0 +1,36 @@
+ford = function(){
+for (let index = 0; index <= 10; index++) {
+    console.log(i);
+}
+
+console.log("Despues del for")
+
+}
+
+pare = function(){
+    for (let i = 2; i <= 20; i+=2) {
+        console.log(i);
+    }
+
+}
+
+mostrarInverso = function(){
+    for (let i = 20; i >=1; i--) {
+        console.log(i);     
+}
+}
+hackeandoNasa = function(){
+    for (let i = 0; i <=100; i+=10) {
+        console.log("Hackeando nasa"+i);  
+}
+}
+
+mostrarImpares = function(){
+    console.log("antes del for");  
+
+    for (let i = 1; i <=21; i+=2) {
+        console.log("impares"+i);  
+}
+console.log("Despues del for");  
+    
+}
